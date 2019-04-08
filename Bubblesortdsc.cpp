e24272c72c2ed49c0821fea80dee098b985d3ef7
muhnuryusri <49118104+muhnuryusri@users.noreply.github.com>
2019-04-08 20:46:39 +0800

    Update Bubblesortdsc.cpp
    Kesalahan pengetikan variabel ada line 14

--- Bubblesortdsc.cpp
+++ Bubblesortdsc.cpp
@@ -11,7 +11,7 @@ void swap(int *xp, int *yp)
 void bubbleSort(int arr[], int n)
 {
   int i, j;
-  for (i = 0; 1 < n-1; i++)
+  for (i = 0; i < n-1; i++)
     for (j = 0; j < n-i-1; j++)
       if (arr[j] > arr[j+1])
       swap (&arr[j], &arr[j+1]);
